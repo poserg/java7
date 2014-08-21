ad27d93c0fe7de07ba47965d300573909db4a3cc
Sergey Popov <poserg@gmail.com>
2014-08-21 10:52:27 +0400

    Чтение ресурса из jar не работает

--- src/main/java/com/github/poserg/App.java
+++ src/main/java/com/github/poserg/App.java
@@ -1,13 +1,22 @@
 package com.github.poserg;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main( final String[] args )
     {
-        System.out.println( "Hello World!" );
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(App.class.getResource("/version.txt").toURI()),
+                StandardCharsets.UTF_8)) {
+            String line = reader.readLine();
+            System.out.println("my version is : " + line);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 }
